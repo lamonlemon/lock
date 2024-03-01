780c96b99fe6946ab3300ca11efe668d5530cf82
lamonlemon <jw.molang@gmail.com>
2024-03-01 12:25:34 +0900

    README Change

--- source/bluetooth_test.uno/bluetooth_test.uno.ino
+++ source/bluetooth_test.uno/bluetooth_test.uno.ino
@@ -2,21 +2,18 @@
 
 SoftwareSerial bluetooth(2, 3); //BlueTooth(Rx, Tx)
 
-boolean isThereCheckSignal = false;
-boolean blueConnect = false;
-unsigned long previousCheckSendTime = 0;
-unsigned long previousPairingCheckTime = 0;
-
 void setup() {
   Serial.begin(9600);
-  bluetooth.begin(9600);
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
+  bluetooth.begin(38400);
 
 }
 
 void loop() {
   if (bluetooth.available()){
     Serial.write(bluetooth.read());
-  }
+  } 
   if (Serial.available()){
     bluetooth.write(Serial.read());
   }
